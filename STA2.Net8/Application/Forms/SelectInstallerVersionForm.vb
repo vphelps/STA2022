a9685ff9b1c2f066f16229735d69b18bc6797b52
vphelps <vphelps@centeredgesoftware.com>
2026-08-04 16:14:27 -0400

    Add installer launch optoins for current and database versions

---
+++ STA2.Net8/Application/Forms/SelectInstallerVersionForm.vb
@@ -0,0 +1,31 @@
+﻿Public Class SelectInstallerVersionForm
+    Public ReadOnly Property SelectedVersion As String
+        Get
+
+            If lstVersions.SelectedItem Is Nothing Then
+                Return Nothing
+            End If
+
+            Return lstVersions.SelectedItem.ToString()
+
+        End Get
+    End Property
+    Public Sub LoadVersions(
+    versions As IEnumerable(Of String)
+)
+
+        lstVersions.Items.Clear()
+
+        For Each version In versions
+            lstVersions.Items.Add(version)
+        Next
+
+        If lstVersions.Items.Count > 0 Then
+            lstVersions.SelectedIndex = 0
+        End If
+
+    End Sub
+    Private Sub SelectInstallerVersionForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
