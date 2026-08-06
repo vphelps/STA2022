ee09814c9badea689b531166543993d8fc9e0ef3
vphelps <vphelps@centeredgesoftware.com>
2026-08-06 17:18:38 -0400

    Add initial database unit tests

---
+++ STA2.Tests/DatabaseHealth.vb
@@ -0,0 +1,15 @@
+﻿Imports STA2.Net8
+Imports Xunit
+
+Public Class DatabaseHealthTests
+
+    <Fact>
+    Public Sub NewDatabaseHealth_IsOfflineByDefault()
+
+        Dim health As New DatabaseHealth()
+
+        Assert.False(health.IsOnline)
+
+    End Sub
+
+End Class
