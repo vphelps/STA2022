bdd8d92e948ad918cf37b3b2c2f228890dd60c7d
vphelps <vphelps@centeredgesoftware.com>
2026-08-06 17:27:47 -0400

    Add database health cache unit tests

--- STA2.Tests/DatabaseServiceTests.vb
+++ STA2.Tests/DatabaseServiceTests.vb
@@ -21,5 +21,45 @@ Public Class DatabaseServiceTests
         Assert.False(result.IsOnline)
 
     End Function
+    <Fact>
+    Public Async Function UsesCachedHealthResult() As Task
 
+        ' Arrange
+        Dim factory As New CountingConnectionFactory()
+
+        Dim service As New DatabaseService(factory)
+
+        ' Act
+        Dim result1 =
+        Await service.EvaluateDatabaseAvailabilityAsync(
+            CancellationToken.None)
+
+        Dim result2 =
+        Await service.EvaluateDatabaseAvailabilityAsync(
+            CancellationToken.None)
+
+        ' Assert
+        Assert.Equal(1, factory.CallCount)
+
+    End Function
+    <Fact>
+    Public Async Function CacheInvalidationForcesNewCheck() As Task
+
+        ' Arrange
+        Dim factory As New CountingConnectionFactory()
+
+        Dim service As New DatabaseService(factory)
+
+        Await service.EvaluateDatabaseAvailabilityAsync(
+            CancellationToken.None)
+
+        service.InvalidateHealthCache()
+
+        Await service.EvaluateDatabaseAvailabilityAsync(
+            CancellationToken.None)
+
+        ' Assert
+        Assert.Equal(2, factory.CallCount)
+
+    End Function
 End Class
