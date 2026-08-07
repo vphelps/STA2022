db7e633e2f010d471e319e5fe5d1a849849ed1f3
vphelps <vphelps@centeredgesoftware.com>
2026-08-07 19:42:33 -0400

    Extract license data refresh logic

--- STA2.Net8/Application/Services/RefreshService.vb
+++ STA2.Net8/Application/Services/RefreshService.vb
@@ -1,3 +1,31 @@
 ﻿Public Class RefreshService
+    Public Function LoadLicenseData() As LicenseDataResult
+
+        Dim dsLic As DataSet =
+        SafeDb.TryQuery(GeneralQueries.LicenseData)
+
+        If dsLic Is Nothing OrElse
+       dsLic.Tables.Count = 0 OrElse
+       dsLic.Tables(0).Rows.Count = 0 Then
+
+            Throw New Exception(
+            "LicenseData returned no rows.")
+
+        End If
+
+        AppData.dbLicData = dsLic
+
+        Dim r = dsLic.Tables(0).Rows(0)
+
+        Return New LicenseDataResult With {
+        .LocationName = r("LocName").ToString(),
+        .LicenseServer = r("LicenseServer").ToString(),
+        .CoreServer = r("CoreServiceServerName").ToString(),
+        .DatabaseVersion = r("Version").ToString(),
+        .WebEnabled = r("EnableWeb").ToString(),
+        .ShiftDate = r("ShiftDate").ToString()
+    }
+
+    End Function
 
 End Class
