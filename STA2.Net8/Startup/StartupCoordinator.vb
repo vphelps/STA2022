8cac1bb578c651ad3b0481d7932fb1c4a5d4cb96
vphelps <vphelps@centeredgesoftware.com>
2026-08-07 17:14:29 -0400

    Add startup coordinator

--- STA2.Net8/Startup/StartupCoordinator.vb
+++ STA2.Net8/Startup/StartupCoordinator.vb
@@ -1,3 +1,13 @@
 ﻿Public Class StartupCoordinator
 
+    Private ReadOnly _form As FormMain
+
+    Public Sub New(form As FormMain)
+
+        _form = form
+
+    End Sub
+    Public Sub Initialize()
+
+    End Sub
 End Class
